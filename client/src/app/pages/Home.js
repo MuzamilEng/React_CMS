3ec8bc1541b75993ba0cc48acd36775018a13b45
Muzamil <124368687+MuzamilEng@users.noreply.github.com>
2023-11-01 14:16:48 +0500

    initial commit

---
+++ client/src/app/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <main className='main_container'>
+        <img src='/images/main.jpg' alt='logo' />
+        <div>
+          <h1>Welcome Back!</h1>
+          .
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default Home
